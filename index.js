1d8fb80fed8b6076d7acf3f15ce4542c8931fb46
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-05 14:03:40 -0800

    interface added with required

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ async function main() {
   console.log("CONNECTED!");
 }
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  specialWord: String,
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  specialWord: {
+    type: String,
+    required: true
+  }
 });
 
 const userdata = mongoose.model("userdata", userSchema);
