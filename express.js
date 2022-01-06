e1406ce6f0f41b48cfe515865b878fb3cc521538
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-05 20:22:16 -0800

    Update express.js

--- express.js
+++ express.js
@@ -1,1 +1,14 @@
-const express = require('express')
+const express = require('express');
+const app = express ();
+const path = require ('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engiene', 'ejs');
+
+app.get('/dog', (req, res) =>{
+    res.send('WoooF')
+})
+
+app.listen(3000, () =>{
+    console.log('App on port 3000!')
+})
