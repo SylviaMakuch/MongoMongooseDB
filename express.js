da3035ab818f67e47cf684993b73526366952ad9
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-07 14:53:17 -0800

    Update express.js

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express ();
 const path = require ('path');
+const mongoose = require("mongoose");
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engiene', 'ejs');
