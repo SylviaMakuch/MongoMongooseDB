23083f50d6f855fb1cb56d5ec691af3ede186cb3
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-07 15:37:42 -0800

    Update express.js

--- express.js
+++ express.js
@@ -3,6 +3,17 @@ const app = express ();
 const path = require ('path');
 const mongoose = require("mongoose");
 
+main().catch((err) => console.log(err));
+
+async function main() {
+    const mongoose = await mongoose.connect("mongodb://localhost:27017/userdata");
+      if (!mongoose) {
+        console.log("Failed Connection");
+      } else {
+        console.log("CONNECTED!");
+      }
+    };
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engiene', 'ejs');
 
