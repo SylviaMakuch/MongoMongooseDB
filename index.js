f9bcccf6974ca7b0b28f314e53e35032b5d5ea69
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-07 19:29:28 -0800

    re-named

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
+const express = require('express');
+const app = express ();
+const path = require ('path');
 const mongoose = require("mongoose");
-main().catch((err) => console.log(err));
+__dirname = path.resolve(path.dirname(''));
 
-async function main() {
-  await mongoose.connect("mongodb://localhost:27017/userdata");
-  console.log("CONNECTED!");
-}
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  email:{
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  specialWord: {
-    type: String,
-    required: true
-  }
-});
+mongoose.connect('mongodb://localhost:27017/catnip', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongo Connection Open")
+    })
+    .catch(err => {
+        console.log("Connection Error!")
+        console.log(err)
+})
 
-const userdata = mongoose.model("userdata", userSchema);
-const user1 = new userdata({
-  firstName: "Sylvia",
-  lastName: "Makuch",
-  email: "sylviamakuch2@gmail.com",
-  password: "$am#ot222.",
-  specialWord: "Tobiisabadboi",
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engiene', 'ejs');
+
+app.get('/cat', (req, res) =>{
+    res.send('Meow')
+})
+
+app.listen(3000, () =>{
+    console.log('App on port 3000!')
+})
