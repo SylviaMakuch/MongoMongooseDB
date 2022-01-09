944d21f3070f195de5d5f42e8945d2c2885d4ddc
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-08 19:25:45 -0800

    added folders

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const app = express ();
-const path = require ('path');
+const express = require("express");
+const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
-const Product = require('./product');
-const { createUnzip } = require('zlib');
-__dirname = path.resolve(path.dirname(''));
+const Product = require("./product");
+const { createUnzip } = require("zlib");
+__dirname = path.resolve(path.dirname(""));
 
-mongoose.connect('mongodb://localhost:27017/catnip', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Mongo Connection Open")
-    })
-    .catch(err => {
-        console.log("Connection Error!")
-        console.log(err)
-})
+mongoose
+  .connect("mongodb://localhost:27017/catnip", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Mongo Connection Open");
+  })
+  .catch((err) => {
+    console.log("Connection Error!");
+    console.log(err);
+  });
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engiene', 'ejs');
+app.set('view engine', 'ejs');
 
-app.get('/products', async (req, res) =>{
-    const products = await Product.find({})
-    console.log (products)
-    res.send('All products will be here ')
-})
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  console.log(products);
+  res.render('index', {products});
+});
 
-app.listen(3000, () =>{
-    console.log('App on port 3000!')
-})
+app.listen(3000, () => {
+  console.log("App on port 3000!");
+});
