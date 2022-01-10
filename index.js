d381fc989a3258c438d4915571514b8239deccb1
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-10 15:51:03 -0800

    working routes with data base

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Product = require("./model/product");
+const { createUnzip } = require("zlib");
 __dirname = path.resolve(path.dirname(""));
+app.use(express.urlencoded({extended: true}))
 
 mongoose
   .connect("mongodb://localhost:27017/catniptypes", {
@@ -27,15 +29,21 @@ app.get("/products", async (req, res) => {
   res.render("index", { products });
 });
 
-app.get('/product/new',(req, res) => {
-    res.render('/products/new')
+app.get('products/new', (req, res) => {
+  res.render("./products/new");
+});
+
+app.post('/products', (req, res)=>{
+    res.send('making it for you')
+    console.log(req.body)
+    // we dont have acess from the body,we need  parse first .: add the url encoded to tell express to use the middle ware here 
 })
 
-app.get('/products/:id', async (req, res) => {
+app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   console.log(product);
-  res.render("/products/show", { product });
+  res.render("./products/show", { product });
 });
 
 app.listen(3000, () => {
