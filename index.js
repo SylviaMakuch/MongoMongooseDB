21bf9908225b9cc8b941e12bf9c6e9918ef6a5de
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-12 17:56:27 -0800

    edit form added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const Product = require("./model/product");
 const { createUnzip } = require("zlib");
 __dirname = path.resolve(path.dirname(""));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect("mongodb://localhost:27017/catniptypes", {
@@ -29,22 +29,30 @@ app.get("/products", async (req, res) => {
   res.render("index", { products });
 });
 
-app.get('/products/new', (req, res) => {
-    res.render('products/new.ejs' )
-})
-
-app.post('/products', async (req, res) => {
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-    res.redirect(`/products/${newProduct._id}`)
-})
-
 app.get("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.render("./products/show", { product });
+  });
+
+app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  console.log(product);
-  res.render("./products/show", { product });
+  res.render("./products/edit",{ product });
 });
+
+// app.get('/products/new', (req, res) => {
+//     res.render('products/new.ejs' )
+// })
+
+// app.post('/products', async (req, res) => {
+//     const newProduct = new Product(req.body);
+//     await newProduct.save();
+//     res.redirect(`/products/${newProduct._id}`)
+// })
+
+
 
 app.listen(3000, () => {
   console.log("App on port 3000!");
